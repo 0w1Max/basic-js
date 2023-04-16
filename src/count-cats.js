b7bd4677991ffe86250142f9b954ebd5b12fd620
Ow1Max <0w1.go.0w1@gmail.com>
2023-04-16 13:10:18 +0300

    feat: Completed the task count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  let count = 0;
+  
+  backyard.forEach(arr => {
+    arr.forEach(item => {
+      if (item === '^^') {
+        count++;
+      }
+    })
+  })
+
+  return count;
 }
 
 module.exports = {
